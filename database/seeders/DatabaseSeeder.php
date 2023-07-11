b8d7ac823528d9eae0497adb07eb91ec4281c809
Mior Muhammad Zaki <crynobone@gmail.com>
2023-07-11 08:36:49 +0800

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
+        $this->call(UserTableSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(MetaSeeder::class);
     }
